1f61c3ef57e6d17bdd1fae27fe04d8adfb30a02b
Ashish Pandey <54909877+ashishpandey608@users.noreply.github.com>
2024-03-04 11:47:26 +0000

    datatypes summary

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+// console.log(typeof null);
+// const id=Symbol('123');
+// // const idEmp=Symbol('123');
+// console.log(id==idEmp);
+const obj={
+    name:'Ashish',
+    id:'21',
+    course:'CSIT',
+    subs:[101,102,103],
+}
+const arr=[2,3,4,5,6,67];
+var age=function myAge(num){
+    return num;
+}
+//
+console.log(typeof obj.subs);
+console.table([typeof arr,typeof age,typeof obj]);
